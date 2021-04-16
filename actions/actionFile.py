18e9d15e54f3844af0e249332a8f568b19acbbc8
Tarun-sngh <67622498+Tarun-sngh@users.noreply.github.com>
2021-04-16 22:31:30 +0530

    Add files via upload

--- actions/actionFile.py
+++ actions/actionFile.py
@@ -1,25 +1,49 @@
+
+
+
+
+# # Making Connection
+
+
+
+# # database
+
+
+# # Created or Switched to collection
+# # names: GeeksForGeeks
+# Collection = db["sensorData"]
+
+
+# def insert(data):
+
+#     # Inserting the loaded data in the Collection
+#     # if JSON contains data more than one entry
+#     # insert_many is used else inser_one is used
+#     # if isinstance(data, list):
+#     #     Collection.insert_many(data)
+#     # else:
+    
+
+
+import sys
 import json
 from pymongo import MongoClient
 import dns
 
-# Making Connection
+from st2common.runners.base_action import Action
+
+
 uri = 'mongodb+srv://new_use:5TAnapiwTBJhkUf@cluster0.gaeaz.mongodb.net/register?retryWrites=true&w=majority'
-myclient = MongoClient(uri)
-
-# database
-db = myclient["stackstorm"]
-
-# Created or Switched to collection
-# names: GeeksForGeeks
-Collection = db["sensorData"]
 
 
-def insert(data):
+class MyEchoAction(Action):
+    def run(self, json):
+        print(json)
+        myclient = MongoClient(uri)
+        db = myclient["stackstorm"]
+        Collection = db["sensorData"]
 
-    # Inserting the loaded data in the Collection
-    # if JSON contains data more than one entry
-    # insert_many is used else inser_one is used
-    # if isinstance(data, list):
-    #     Collection.insert_many(data)
-    # else:
-    Collection.insert_one(data)
+        if json:
+            Collection.insert_one(json)
+        return (False, "No Data")
+
