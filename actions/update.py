00ed87e03316086ffdc6e23d9e1121c3180edf9d
Tarun-sngh <67622498+Tarun-sngh@users.noreply.github.com>
2021-04-19 20:12:25 +0530

    Add files via upload

--- actions/update.py
+++ actions/update.py
@@ -10,14 +10,14 @@ from st2common.runners.base_action import Action
 
 
 class MyEchoAction(Action):
-    def run(self, data, id, uri):
+    def run(self, data, iD, uri):
 
         myclient = MongoClient(uri)
         db = myclient["stackstorm"]
         collection = db["sensorData"]
                 
         collection.find_one_and_update(
-            {"_id" : ObjectId(id)},
+            {"_id" : ObjectId(iD)},
                 {"$set":data},
                 upsert=True
             )
